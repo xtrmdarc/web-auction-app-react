3b08063cbfb589f997860536207554bf98c0ed82
xtrmdarc <diegoreyesco@hotmail.com>
2020-11-07 00:15:36 -0500

    Add persistance to user state

--- src/store.js
+++ src/store.js
@@ -1,6 +1,19 @@
 import {createStore} from 'redux';
 import rootReducer from './reducers';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
-const store = createStore(rootReducer);
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: autoMergeLevel2,
+  whitelist: ['user'],
+}
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(pReducer);
+export const persistor = persistStore(store);
 
 export default store;
